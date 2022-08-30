ebc11623b6fc518ee0ed108f33b451f595fdaba5
thomashazekamp <thomas.hazekamp@gmail.com>
2022-08-30 18:43:33 +0100

    first question

---
+++ 217-ContainsDuplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        new_sort = sorted(nums)
+        for i in range(len(new_sort) - 1):
+            if new_sort[i] == new_sort[i + 1]:
+                return True
+        return False
+
+# Time: O(n)
+# Space: O(1)
